01063c51ea8f7099e4dded27d039bf4f09ca20fd
Sei <1512120250@qq.com>
2019-02-15 20:52:57 +0800

    链表数据结构（单向双向链表完善，循环链表待加）

--- DataStructure/LinkedStructure.py
+++ DataStructure/LinkedStructure.py
@@ -18,13 +18,20 @@ class TwoWayNode(Node):
         self.previous = previous
 
 class LinkedList(object):
+    node = {
+        "1": Node,
+        "2": TwoWayNode,
+    }
+
     '''链表结构'''
     def __init__(self, type=1, length=10, filldata=None):
         if type == 1:
+            self.type = "1"
             self.head = None
             for i in range(length):
                 self.head = Node(i, self.head)
         elif type == 2:
+            self.type = "2"
             self.head = TwoWayNode(None)
             self.tail = self.head
             for i in range(1, length):
@@ -54,12 +61,60 @@ class LinkedList(object):
             index -= 1
         return probe.data
 
+    def pop(self):
+        '''删除最后一个节点的数据，返回其节点数据值'''
+        if self.type == "1":
+            probe = self.head
+            while probe.next.next != None:
+                probe = probe.next
+            removedata = probe.next.data
+            probe.next = None
+        elif self.type == "2":
+            removedata = self.tail.data
+            self.tail = self.tail.previous
+            self.tail.next = None
+        return removedata
+
+
+
     def replace(self, index, newdata):
+        '''替换指定位置的data值'''
         probe = self.head
         while index > 0:
             probe = probe.next
             index -= 1
         probe.data = newdata
+
+    def insert(self, index, newdata):
+        '''在指定位置插入某值'''
+        if self.head == None or index <= 0:
+            if self.type == "1":
+                self.head = self.node[self.type](newdata, self.head)
+            elif self.type == "2":
+                self.head.next = self.node[self.type](newdata, self.head, self.head.next)
+        else:
+            if self.type == "1":
+                probe = self.head
+                while probe.next != None and index > 1:
+                    probe = probe.next
+                    index -= 1
+                probe.next = self.node[self.type](newdata, probe.next)
+            elif self.type == "2":
+                probe = self.head
+                while probe.next != None and index > 1:
+                    probe = probe.next
+                    index -= 1
+                probe.next = self.node[self.type](newdata, probe, probe.next)
+
+    def tolist(self):
+        targetList = list()
+        for i in self:
+            targetList.append(i)
+        return targetList
+
+class CircularList(object):
+    def __init__(self):
+        pass
 
 
 
@@ -84,6 +139,6 @@ if __name__ == '__main__':
     # while probe != None:
     #     print probe.data
     #     probe = probe.previous
-    a = LinkedList(1, 5, 6)
+    a = LinkedList(type=2)
     for i in a:
         print i
